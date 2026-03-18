1260dffb781392eab7bfbd1b78a36d72a2e7b32d
kennedy ochuko <kennedykenney082@gmail.com>
2026-03-18 12:09:40 +0100

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
-import MessageRoutes from './routes/message.route.js'
+import MessageRoutes from './routes/message.route.js';
+import path from 'path'
 
 
 dotenv.config();
 
 const app = express();
+const _dirname = path.resolve()
 
 const PORT = process.env.PORT;
 
 app.use('/api/auth',authRoutes)
-app.use('/api/messages',MessageRoutes)
+app.use('/api/messages',MessageRoutes);
+
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(_dirname, '../frontend/dist')));
+    app.get('/', (req, res)=>{
+        res.sendFile(path.join(_dirname, '../frontend','dist','index.html'))
+    })
+}
 
 
 app.listen(PORT, ()=>{
